5248a36e34e574a4a607abf82f6622dec7e69f27
Matt Huntington <matt.huntington@gmail.com>
2017-09-20 16:47:25 -0400

    dynamic db config

--- database/db.php
+++ database/db.php
@@ -1,6 +1,6 @@
 <?php
 $dbconn = null;
-if(!getenv('DATABASE_URL')){
+if(getenv('DATABASE_URL')){
     $connectionConfig = parse_url(getenv('DATABASE_URL'));
     $host = $connectionConfig['host'];
     $user = $connectionConfig['user'];
