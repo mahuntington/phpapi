bdcdcea14b9f6af29e5fb812767ecbeea7d85189
Matt Huntington <matt.huntington@gmail.com>
2017-09-20 11:21:37 -0400

    moved create to controller file

--- controllers/people.php
+++ controllers/people.php
@@ -4,5 +4,13 @@
 
     if($_REQUEST['action'] === 'index'){
         echo json_encode($people);
+    } else if ($_REQUEST['action'] === 'post'){
+        $requestBody = file_get_contents('php://input');
+        $body = json_decode($requestBody);
+        $zagthar = new Person($body->name, $body->age);
+
+        $people[] = $zagthar;
+
+        echo json_encode($people);
     }
 ?>
