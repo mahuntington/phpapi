91b140003028b8602b0bc9764fae8d476f9eed94
Matt Huntington <matt.huntington@gmail.com>
2017-09-20 12:15:04 -0400

    formatting

--- controllers/people.php
+++ controllers/people.php
@@ -1,26 +1,26 @@
 <?php
-    header('Content-Type: application/json');
-    include __DIR__ . '/../models/person.php';
+header('Content-Type: application/json');
+include __DIR__ . '/../models/person.php';
 
-    if($_REQUEST['action'] === 'index'){
-        echo json_encode(People::find());
-    } else if ($_REQUEST['action'] === 'post'){
-        $requestBody = file_get_contents('php://input');
-        $body = json_decode($requestBody);
-        $newPerson = new Person($body->name, $body->age);
+if($_REQUEST['action'] === 'index'){
+    echo json_encode(People::find());
+} else if ($_REQUEST['action'] === 'post'){
+    $requestBody = file_get_contents('php://input');
+    $body = json_decode($requestBody);
+    $newPerson = new Person($body->name, $body->age);
 
-        $allPeople = People::create($newPerson);
+    $allPeople = People::create($newPerson);
 
-        echo json_encode($allPeople);
-    } else if ($_REQUEST['action'] === 'delete'){
-        $allPeople = People::delete($_REQUEST['id']);
-        echo json_encode($allPeople);
-    } else if ($_REQUEST['action'] === 'update'){
-        $requestBody = file_get_contents('php://input');
-        $body = json_decode($requestBody);
-        $updatedPerson = new Person($body->name, $body->age);
-        $allPeople = People::update($_REQUEST['id'], $updatedPerson);
+    echo json_encode($allPeople);
+} else if ($_REQUEST['action'] === 'delete'){
+    $allPeople = People::delete($_REQUEST['id']);
+    echo json_encode($allPeople);
+} else if ($_REQUEST['action'] === 'update'){
+    $requestBody = file_get_contents('php://input');
+    $body = json_decode($requestBody);
+    $updatedPerson = new Person($body->name, $body->age);
+    $allPeople = People::update($_REQUEST['id'], $updatedPerson);
 
-        echo json_encode($allPeople);
-    }
+    echo json_encode($allPeople);
+}
 ?>
